8113a1006d2f0359eefa983e8f5a0dbb9ace500d
Ryan Petschek <petschekr@gmail.com>
2024-09-28 02:46:46 -0400

    Send raw data over Comma CAN instead of partially parsing + add HVAC + add BME280 environmental data

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 #![no_std]
 #![no_main]
 
-use bme280::i2c::BME280;
+use bme280_rs::AsyncBme280;
 use defmt::*;
 use embassy_embedded_hal::shared_bus::asynch::spi::SpiDevice;
 use embassy_executor::Spawner;
 use embassy_rp::gpio::{Input, Level, Output, Pull};
 use embassy_rp::i2c;
-use embassy_rp::peripherals::SPI0;
+use embassy_rp::peripherals::{SPI0, I2C0};
 use embassy_rp::spi::{self, Spi};
 use embassy_sync::blocking_mutex::raw::CriticalSectionRawMutex;
 use embassy_sync::mutex::Mutex;
@@ -25,9 +25,13 @@ use {defmt_rtt as _, panic_probe as _};
 type SPI0Type<BUS> = Spi<'static, BUS, spi::Async>;
 static SPI_BUS0: StaticCell<Mutex<CriticalSectionRawMutex, SPI0Type<SPI0>>> = StaticCell::new();
 
-static FORWARDING_CHANNEL: Channel<CriticalSectionRawMutex, obd_data::Data, 10> = Channel::new();
+static FORWARDING_CHANNEL: Channel<CriticalSectionRawMutex, (StandardId, Vec<u8, 64>), 10> = Channel::new();
 
 static OBD_CONTROLLER: StaticCell<Mutex<CriticalSectionRawMutex, MCP25xxFD<SpiDevice<CriticalSectionRawMutex, SPI0Type<SPI0>, Output>>>> = StaticCell::new();
+
+embassy_rp::bind_interrupts!(struct Irqs {
+    I2C0_IRQ => i2c::InterruptHandler<I2C0>;
+});
 
 fn construct_uds_query(command: &[u8]) -> [u8; 8] {
     let mut query = [0u8; 8];
@@ -44,97 +48,34 @@ fn construct_uds_query(command: &[u8]) -> [u8; 8] {
 struct ECUAddresses {
     bms: Id,
     tpms: Id,
+    hvac: Id,
 }
 impl ECUAddresses {
     fn new() -> (Self, Self) {
         let tx = Self {
             bms: StandardId::new(0x7E4).unwrap().into(),
             tpms: StandardId::new(0x7A0).unwrap().into(),
+            hvac: StandardId::new(0x7B3).unwrap().into(),
         };
         let rx = Self {
             bms: Self::rx_address(tx.bms),
             tpms: Self::rx_address(tx.tpms),
+            hvac: Self::rx_address(tx.hvac),
         };
         (tx, rx)
     }
-    fn rx_address(ecu_addr: impl Into<Id>) -> Id {
+    fn address_offset<const O: i32>(ecu_addr: impl Into<Id>) -> Id {
         let ecu_addr = ecu_addr.into();
         match ecu_addr {
-            Id::Standard(addr) => StandardId::new(addr.as_raw() + 8).unwrap().into(),
-            Id::Extended(addr) => ExtendedId::new(addr.as_raw() + 8).unwrap().into(),
+            Id::Standard(addr) => StandardId::new(((addr.as_raw() as i32) + O) as u16).unwrap().into(),
+            Id::Extended(addr) => ExtendedId::new(((addr.as_raw() as i32) + O) as u32).unwrap().into(),
         }
     }
-}
-
-mod obd_data {
-    use defmt::Format;
-    use serde::{Deserialize, Serialize};
-
-    #[derive(Serialize, Deserialize, Format)]
-    pub enum ChargingType {
-        NotCharging,
-        AC,
-        DC,
-        Other,
+    fn rx_address(ecu_addr: impl Into<Id>) -> Id {
+        Self::address_offset::<8>(ecu_addr)
     }
-    #[derive(Serialize, Deserialize, Format)]
-    pub struct BatteryData1 {
-        pub charging: ChargingType,
-        pub bms_ignition: bool,
-        pub bms_relay: bool,
-        pub aux_battery_voltage: u8,
-
-        pub bms_soc: u8,
-        pub battery_current: i16,
-        pub battery_voltage: u16,
-
-        pub fan_speed: u8,
-        pub fan_feedback: u8,
-
-        pub cumulative_energy_charged: u32,
-        pub cumulative_energy_discharged: u32,
-        pub cumulative_charge_current: u32,
-        pub cumulative_discharge_current: u32,
-
-        pub cumulative_operating_time: u32,
-
-        pub available_charge_power: u16,
-        pub available_discharge_power: u16,
-
-        pub dc_battery_inlet_temp: i8,
-        pub dc_battery_max_temp: i8,
-        pub dc_battery_min_temp: i8,
-        pub dc_battery_cell_max_voltage: u8,
-        pub dc_battery_cell_max_voltage_id: u8,
-        pub dc_battery_cell_min_voltage: u8,
-        pub dc_battery_cell_min_voltage_id: u8,
-
-        pub rear_drive_motor_speed: i16,
-        pub front_drive_motor_speed: i16,
-        pub dc_battery_module_temp1: i8,
-        pub dc_battery_module_temp2: i8,
-        pub dc_battery_module_temp3: i8,
-        pub dc_battery_module_temp4: i8,
-        pub dc_battery_module_temp5: i8,
-    }
-    #[derive(Serialize, Deserialize, Format)]
-    pub struct TirePressures {
-        pub front_left_psi: u8,
-        pub front_left_temp: u8, // Subtract 55 degrees C
-        pub front_right_psi: u8,
-        pub front_right_temp: u8,
-        pub rear_left_psi: u8,
-        pub rear_left_temp: u8,
-        pub rear_right_psi: u8,
-        pub rear_right_temp: u8,
-    }
-
-    #[derive(Serialize, Deserialize, Format)]
-    pub enum Data {
-        BatteryData1(BatteryData1),
-        TirePressures(TirePressures),
-        CabinEnvironmentData(()), // TODO
-        Error(&'static str),
+    fn tx_address(ecu_addr: impl Into<Id>) -> Id {
+        Self::address_offset::<-8>(ecu_addr)
     }
 }
 
@@ -167,17 +108,17 @@ async fn main(spawner: Spawner) {
     let mut comma_stby = Output::new(p.PIN_25, Level::Low);
     comma_stby.set_low();
 
-    let i2c = i2c::I2c::new_blocking(p.I2C0, p.PIN_1, p.PIN_0, i2c::Config::default());
-    let mut bme280 = BME280::new_primary(i2c);
-    bme280.init(&mut Delay).unwrap();
+    let i2c = i2c::I2c::new_async(p.I2C0, p.PIN_1, p.PIN_0, Irqs, i2c::Config::default());
 
     spawner.must_spawn(obd_task(spawner, spi0, obd_cs, obd_int));
+    spawner.must_spawn(bme_sender_task(i2c));
     spawner.must_spawn(comma_task(spi0, comma_cs, comma_int));
 }
 
 const TRANSMIT_FIFO: u8 = 1;
 const RX_BATTERY_FIFO: u8 = 2;
 const RX_TPMS_FIFO: u8 = 3;
+const RX_HVAC_FIFO: u8 = 4;
 
 #[embassy_executor::task]
 async fn obd_task(spawner: Spawner, spi_bus: &'static Mutex<CriticalSectionRawMutex, SPI0Type<SPI0>>, cs: Output<'static>, mut int: Input<'static>) {
@@ -204,7 +145,7 @@ async fn obd_task(spawner: Spawner, spi_bus: &'static Mutex<CriticalSectionRawMu
         ).await.unwrap();
 
         obd_controller.configure_fifo(
-            FIFOConfig::<RX_BATTERY_FIFO>::rx_with_size(16, PayloadSize::Bytes8)
+            FIFOConfig::<RX_BATTERY_FIFO>::rx_with_size(8, PayloadSize::Bytes8)
         ).await.unwrap();
         obd_controller.configure_filter(
             FilterConfig::<RX_BATTERY_FIFO, RX_BATTERY_FIFO>::from_id(rx_addrs.bms),
@@ -212,11 +153,19 @@ async fn obd_task(spawner: Spawner, spi_bus: &'static Mutex<CriticalSectionRawMu
         ).await.unwrap();
 
         obd_controller.configure_fifo(
-            FIFOConfig::<RX_TPMS_FIFO>::rx_with_size(16, PayloadSize::Bytes8)
+            FIFOConfig::<RX_TPMS_FIFO>::rx_with_size(8, PayloadSize::Bytes8)
         ).await.unwrap();
         obd_controller.configure_filter(
             FilterConfig::<RX_TPMS_FIFO, RX_TPMS_FIFO>::from_id(rx_addrs.tpms),
             MaskConfig::<RX_TPMS_FIFO>::match_exact(),
+        ).await.unwrap();
+
+        obd_controller.configure_fifo(
+            FIFOConfig::<RX_HVAC_FIFO>::rx_with_size(8, PayloadSize::Bytes8)
+        ).await.unwrap();
+        obd_controller.configure_filter(
+            FilterConfig::<RX_HVAC_FIFO, RX_HVAC_FIFO>::from_id(rx_addrs.hvac),
+            MaskConfig::<RX_HVAC_FIFO>::match_exact(),
         ).await.unwrap();
 
         obd_controller.set_mode(registers::OperationMode::Normal).await.unwrap();
@@ -233,19 +182,19 @@ async fn obd_task(spawner: Spawner, spi_bus: &'static Mutex<CriticalSectionRawMu
         let mut iso_tp_length: Option<u16> = None;
         let mut current_fifo: Option<u8> = None;
 
-        let (id, pid, data) = loop {
-            let rx_result = {
-                obd_controller.lock().await.receive(current_fifo).await
-            };
+        let (frame, pid, data) = loop {
+            // Temp variable required to not hold lock across a different await point
+            let rx_result = obd_controller.lock().await.receive(current_fifo).await;
+
             match rx_result {
-                Ok((fifo, frame)) => {
+                Ok(Some((fifo, frame))) => {
                     current_fifo = Some(fifo);
-                    debug!("Received message from FIFO{}: {:x} ({} bytes): {:x}", fifo, frame.raw_id(), frame.data().len(), frame.data());
+                    trace!("Received message from FIFO{}: {:x} ({} bytes): {:x}", fifo, frame.raw_id(), frame.data().len(), frame.data());
 
                     match frame.data()[0] >> 4 {
                         0 => {
                             // Single ISO-TP frame
-                            debug!("Single frame of data");
+                            trace!("Single frame of data");
                             // ISO-TP transmission complete
                             iso_tp_data.extend_from_slice(&frame.data()[1..]).unwrap();
                             break (frame, &iso_tp_data[1..3], &iso_tp_data[3..]); // Strip 0x62 (UDS response) + PID (2 bytes) from data
@@ -253,17 +202,13 @@ async fn obd_task(spawner: Spawner, spi_bus: &'static Mutex<CriticalSectionRawMu
                         1 => {
                             // First ISO-TP frame
                             let length = frame.data()[1] as u16 + ((frame.data()[0] as u16 & 0b1111) << 8);
-                            debug!("First frame of data with total length {}", length);
+                            trace!("First frame of data with total length {}", length);
                             iso_tp_length = Some(length);
                             iso_tp_data.clear();
                             iso_tp_data.extend_from_slice(&frame.data()[2..]).unwrap();
 
                             // Send flow control message
-                            let tx_id: Id = match frame.id() {
-                                Id::Standard(id) => StandardId::new(id.as_raw() - 8).unwrap().into(),
-                                Id::Extended(id) => ExtendedId::new(id.as_raw() - 8).unwrap().into(),
-                            };
-                            let flow_control_frame = Frame::new(tx_id, &[0x30, 0x00, 10, 0x00, 0x00, 0x00, 0x00, 0x00]).unwrap();
+                            let flow_control_frame = Frame::new(ECUAddresses::tx_address(frame.id()), &[0x30, 0x00, 10, 0x00, 0x00, 0x00, 0x00, 0x00]).unwrap();
                             obd_controller
                                 .lock().await
                                 .transmit::<TRANSMIT_FIFO>(&flow_control_frame).await
@@ -272,7 +217,7 @@ async fn obd_task(spawner: Spawner, spi_bus: &'static Mutex<CriticalSectionRawMu
                         2 => {
                             // Consecutive ISO-TP frame
                             let frame_number = frame.data()[0] & 0b1111;
-                            debug!("Consecutive frame #{}", frame_number);
+                            trace!("Consecutive frame #{}", frame_number);
                             iso_tp_data.extend_from_slice(&frame.data()[1..]).unwrap();
 
                             if iso_tp_data.len() as u16 >= iso_tp_length.unwrap_or(u16::MAX) {
@@ -283,85 +228,39 @@ async fn obd_task(spawner: Spawner, spi_bus: &'static Mutex<CriticalSectionRawMu
                         _ => {},
                     }
                 },
-                Err(mcp25xxfd::Error::ControllerError(description)) => {
-                    FORWARDING_CHANNEL.send(obd_data::Data::Error(description)).await;
+                Ok(None) => {
+                    int.wait_for_low().await
                 },
-                err => { err.unwrap(); },
+                Err(mcp25xxfd::Error::ControllerError(description)) => {
+                    error!("{}", description);
+                    FORWARDING_CHANNEL.send((StandardId::new(0x700).unwrap(), Vec::from_slice(description.as_bytes()).unwrap())).await;
+                },
+                Err(err) => { dbg!(err); },
             }
         };
 
-        let data = match id {
-            frame if frame.id() == rx_addrs.bms => Some(process_battery_data(data)),
-            frame if frame.id() == rx_addrs.tpms => Some(process_tpms_data(data)),
-            frame => {
+        // rx_addrs
+        let forwarding_address = match frame.id() {
+            addr if addr == rx_addrs.bms => { StandardId::new(0x701).unwrap() },
+            addr if addr == rx_addrs.tpms => { StandardId::new(0x702).unwrap() },
+            addr if addr == rx_addrs.hvac => { StandardId::new(0x703).unwrap() },
+            _ => {
                 warn!("Unhandled ISO-TP response from address {:x} to PID {:x}: {:x}", frame.raw_id(), pid, data);
-                None
+                continue;
             },
         };
-        if let Some(data) = data {
-            FORWARDING_CHANNEL.send(data).await;
-        }
-    }
-
-    fn process_battery_data(data: &[u8]) -> obd_data::Data {
-        obd_data::Data::BatteryData1(obd_data::BatteryData1 {
-            charging:
-                if data[9] & 0x20 > 0 { obd_data::ChargingType::AC }
-                else if data[9] & 0x40 > 0 { obd_data::ChargingType::DC }
-                else if data[9] & 0x80 > 0 { obd_data::ChargingType::Other }
-                else { obd_data::ChargingType::NotCharging },
-            bms_ignition: data[50] & 0x04 > 0,
-            bms_relay: data[9] & 0x01 > 0,
-            bms_soc: data[4],
-            battery_current: i16::from_be_bytes(data[10..12].try_into().unwrap()),
-            battery_voltage: u16::from_be_bytes(data[12..14].try_into().unwrap()),
-            aux_battery_voltage: data[29],
-            fan_speed: data[27],
-            fan_feedback: data[28],
-            cumulative_energy_charged: u32::from_be_bytes(data[38..42].try_into().unwrap()),
-            cumulative_energy_discharged: u32::from_be_bytes(data[42..46].try_into().unwrap()),
-            cumulative_charge_current: u32::from_be_bytes(data[30..34].try_into().unwrap()),
-            cumulative_discharge_current: u32::from_be_bytes(data[34..38].try_into().unwrap()),
-            cumulative_operating_time: u32::from_be_bytes(data[46..50].try_into().unwrap()),
-            available_discharge_power: u16::from_be_bytes(data[5..7].try_into().unwrap()),
-            available_charge_power: u16::from_be_bytes(data[7..9].try_into().unwrap()),
-            dc_battery_inlet_temp: data[22] as i8,
-            dc_battery_max_temp: data[14] as i8,
-            dc_battery_min_temp: data[15] as i8,
-            dc_battery_cell_max_voltage: data[23],
-            dc_battery_cell_max_voltage_id: data[24],
-            dc_battery_cell_min_voltage: data[25],
-            dc_battery_cell_min_voltage_id: data[26],
-            rear_drive_motor_speed: i16::from_be_bytes(data[53..55].try_into().unwrap()),
-            front_drive_motor_speed: i16::from_be_bytes(data[55..57].try_into().unwrap()),
-            dc_battery_module_temp1: data[16] as i8,
-            dc_battery_module_temp2: data[17] as i8,
-            dc_battery_module_temp3: data[18] as i8,
-            dc_battery_module_temp4: data[19] as i8,
-            dc_battery_module_temp5: data[20].try_into().unwrap(), // TODO: same as inlet temp???
-        })
-    }
-
-    fn process_tpms_data(data: &[u8]) -> obd_data::Data {
-        obd_data::Data::TirePressures(obd_data::TirePressures {
-            front_left_psi: data[4],
-            front_left_temp: data[5],
-            front_right_psi: data[9],
-            front_right_temp: data[10],
-            rear_left_psi: data[14],
-            rear_left_temp: data[15],
-            rear_right_psi: data[19],
-            rear_right_temp: data[20],
-        })
+        FORWARDING_CHANNEL.send((forwarding_address, Vec::from_slice(&data).unwrap())).await;
     }
 }
 
 #[embassy_executor::task]
 async fn obd_sender_task(obd_controller: &'static Mutex<CriticalSectionRawMutex, MCP25xxFD<SpiDevice<'_, CriticalSectionRawMutex, SPI0Type<SPI0>, Output<'_>>>>, tx_addrs: ECUAddresses) {
+    // Frame::new(tx_addrs.tpms, &construct_uds_query(&[0xC0, 0x02])).unwrap(), // Tire IDs(?)
+
     let queries = [
         Frame::new(tx_addrs.bms, &construct_uds_query(&[0x01, 0x01])).unwrap(),
-        // Frame::new(tx_addrs.tpms, &construct_uds_query(&[0xC0, 0x02])).unwrap(),
         Frame::new(tx_addrs.tpms, &construct_uds_query(&[0xC0, 0x0B])).unwrap(),
+        Frame::new(tx_addrs.hvac, &construct_uds_query(&[0x01, 0x00])).unwrap(),
     ];
 
     loop {
@@ -371,7 +270,40 @@ async fn obd_sender_task(obd_controller: &'static Mutex<CriticalSectionRawMutex,
                 .lock().await
                 .transmit::<TRANSMIT_FIFO>(frame).await
                 .unwrap();
+            Timer::after_millis(10).await;
         }
+
+        Timer::after_millis(1000).await;
+    }
+}
+
+#[embassy_executor::task]
+async fn bme_sender_task(i2c: i2c::I2c<'static, I2C0, i2c::Async>) {
+    let mut bme280 = AsyncBme280::new(i2c, Delay);
+    bme280.init().await.unwrap();
+    bme280.set_sampling_configuration(
+        bme280_rs::Configuration::default()
+            .with_sensor_mode(bme280_rs::SensorMode::Normal)
+            .with_standby_time(bme280_rs::StandbyTime::Millis500)
+            .with_pressure_oversampling(bme280_rs::Oversampling::Oversample8)
+            .with_temperature_oversampling(bme280_rs::Oversampling::Oversample8)
+            .with_humidity_oversampling(bme280_rs::Oversampling::Oversample8)
+            .with_filter(bme280_rs::Filter::Filter4)
+    ).await.unwrap();
+
+    let mut forward_data: Vec<u8, 64> = Vec::new();
+    loop {
+        forward_data.clear();
+
+        let sample = bme280.read_sample().await.unwrap();
+        let pressure = sample.pressure.unwrap_or(0.0).to_be_bytes();
+        let temperature = sample.temperature.unwrap_or(0.0).to_be_bytes();
+        let humidity = sample.humidity.unwrap_or(0.0).to_be_bytes();
+
+        forward_data.extend_from_slice(&pressure).unwrap();
+        forward_data.extend_from_slice(&temperature).unwrap();
+        forward_data.extend_from_slice(&humidity).unwrap();
+        FORWARDING_CHANNEL.send((StandardId::new(0x720).unwrap(), forward_data.clone())).await;
 
         Timer::after_millis(1000).await;
     }
@@ -400,13 +332,11 @@ async fn comma_task(spi_bus: &'static Mutex<CriticalSectionRawMutex, SPI0Type<SP
     Timer::after_millis(500).await;
 
     loop {
-        let forward_data = FORWARDING_CHANNEL.receive().await;
+        let (forward_addr, forward_data) = FORWARDING_CHANNEL.receive().await;
 
-        let data: Vec<u8, 64> = postcard::to_vec(&forward_data).unwrap();
-        debug!("{:?}", &forward_data);
-        debug!("Serialized length is: {}", data.len());
+        debug!("Forwarding {} bytes to address {:x}", forward_data.len(), forward_addr.as_raw());
 
-        let forward_frame = Frame::new(StandardId::new(0x715).unwrap(), data.as_slice()).unwrap();
+        let forward_frame = Frame::new(forward_addr, forward_data.as_slice()).unwrap();
         comma_controller.transmit::<TRANSMIT_FIFO>(&forward_frame).await.unwrap();
     }
 }
